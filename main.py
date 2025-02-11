1c3a8bdafad9dc1694bcb93b9d08192653997d1b
theMladyPan <stanislav@rubint.sk>
2025-02-11 23:08:11 +0100

    Enhance functionality by adding MLflow integration, updating model parameters, and refining agent tools; include new dependencies in requirements.txt and update .gitignore.

--- main.py
+++ main.py
@@ -12,11 +12,24 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.tools import tool
 from langchain.agents import AgentExecutor, create_tool_calling_agent
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.tools import TavilySearchResults
 import dotenv
 import logging
 import requests
 import os
 import time
+import mlflow
+import datetime
+
+
+# mlflow_uri = "http://192.168.30.21:8080/"
+# mlflow.set_tracking_uri(mlflow_uri)
+date = datetime.datetime.now().strftime("%Y-%m-%d")
+mlflow.set_experiment(f"{date}")
+
+# This will enable autologging for all the components in the LLM chain
+mlflow.langchain.autolog()
+
 
 log = logging.getLogger("uvicorn")
 logging.basicConfig(level=logging.INFO)
@@ -29,7 +42,7 @@ app = FastAPI()
 
 
 # Initialize LangChain agent
-model = ChatOpenAI(model="gpt-4o-mini", temperature=0.3)  # Replace with your API setup
+model = ChatOpenAI(model="gpt-4o-mini", temperature=0.1)  # Replace with your API setup
 
 
 # FastAPI request and response models
@@ -48,7 +61,7 @@ class Response(BaseModel):
 
 # Dummy document loader and vector database setup
 def create_vector_store():
-    loader = TextLoader("dummy_data.txt")  # Replace with your document or dataset
+    loader = TextLoader("data.txt")  # Replace with your document or dataset
     documents = loader.load()
     text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=20)
     texts = text_splitter.split_documents(documents)
@@ -61,49 +74,58 @@ vector_store = create_vector_store()
 
 
 @tool
-def math_agent(query: str):
+async def math_agent(query: str):
     "Useful for solving math problems."
-    log.info(f"### Math Bot: {query}")
-    result = model.invoke(query)
+    result = await model.ainvoke(query)
     return result
 
 
-@tool
-def web_agent(query):
-    """Searches the web for relevant information based on input if not found in knowledge base."""
-    log.info(f"### Web Bot: {query} on web")
-    query = f"https://www.google.com/search?q={query}"
-    html = requests.get(query).text
-    return html
+web_search_tool = TavilySearchResults(
+    max_results=3,
+    search_depth="advanced",
+    include_answer=True,
+    include_raw_content=True,
+    include_images=True,
+    description="Search the web for the user query when needed to provide more information. Use only as a last resort.",
+)
 
 
 @tool
-def weather_agent(query: WeatherQuery):
-    """Provides weather information based on geolocation, requires latitude and longitude."""
+async def weather_agent(query: WeatherQuery):
+    """Provides weather information based on geolocation, requires latitude and longitude. Only use this tool when exact location is well known."""
 
     query = f"https://api.openweathermap.org/data/2.5/forecast?lat={query.latitude}&lon={query.longitude}&appid={os.getenv('OPEN_WEATHER_API_KEY')}"
-    log.info(f"### Weather Bot: Searching for: {query}")
     html = requests.get(query).text
     return html
 
 
 @tool
-def knowledge_agent(query: str):
-    """Use this tool to search for information from the knowledge base.
+async def knowledge_agent(query: str):
+    """Always use this tool first. Usage: to get information from the knowledge base.
     It is helpful when a user asks for specific knowledge, facts, or insights."""
 
-    log.info(f"### Knowledge bot: {query}")
-    response = vector_store.similarity_search(query, k=1)
+    messages = [
+        (
+            "system",
+            "Provide keywords to be searched in knowledge base to extract \
+                unknown information from knowledge base necessary to answer the user question.",
+        ),
+        ("user", f"Question: {query}"),
+    ]
+
+    what_to_search = await model.ainvoke(messages)
+    log.info(f"Searching for: {what_to_search.content}")
+    response = vector_store.similarity_search(what_to_search.content, k=3)
     log.info(f"Found: {response}")
     return response
 
 
-tools = [knowledge_agent, math_agent, weather_agent, web_agent]
+tools = [knowledge_agent, math_agent, weather_agent, web_search_tool]
 # agent_chain = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION)
 
 prompt = ChatPromptTemplate.from_messages(
     [
-        ("system", "You are a helpful RAG assistant, always look into knowledge base first."),
+        ("system", "You are a helpful RAG assistant, always use knowledge base tool first."),
         ("human", "{input}"),
         # Placeholders fill up a **list** of messages
         ("placeholder", "{agent_scratchpad}"),
@@ -127,15 +149,16 @@ async def home():
 
 @app.post("/chat", response_model=Response)
 async def chat(query: Query):
-    tstart = time.time()
-    try:
-        response = await agent_executor.ainvoke({"input": query})
-        # response = agent_executor.invoke({"input": query})
-        tend = time.time()
-        log.info(f"Took: {tend-tstart} seconds")
-        return Response(response=response)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    with mlflow.start_run():
+        try:
+            tstart = time.time()
+            response = await agent_executor.ainvoke({"input": query})
+            # response = agent_executor.invoke({"input": query})
+            tend = time.time()
+            log.info(f"Took: {tend-tstart} seconds")
+            return Response(response=response)
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
 
 
 # Running the application
